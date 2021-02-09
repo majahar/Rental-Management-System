9a55cc5f8bbf0f11793855a6dec77aab8de09349
majaharkhan <majahark.inc@fliplearn.com>
2021-02-09 18:10:25 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { data } from '../assets/catalog.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RMS';
+  results : any;
+  categories: any;
+  subcategories: any;
+  showCategory = false;
+  showSubCategory = false;
+
+
+  constructor() {
+    results : Array;
+
+
+   }
+
+  ngOnInit(): void {
+    this.results = data.locations;
+    this.categories=[];
+
+  }
+  showCategories(branches){
+    this.showCategory = true;
+    this.showSubCategory =false;
+   console.log(branches.categories);
+    this.categories=[];
+    this.categories=branches.categories;
+ }
+ showSubcategory(subcategories){
+  this.showSubCategory =true;
+  this.showCategory = false;
+  this.subcategories=[];
+  this.subcategories=subcategories;
+
+}
+back(){
+  this.showSubCategory =false;
+  this.showCategory = true;
+}
 }
